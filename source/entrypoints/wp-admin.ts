f70891ed4f40a94d109babcfad048b4de083dfc6
AlexanderBorysenko <borysenko.alexander@gmail.com>
2024-05-16 19:49:20 +0300

    added script to improve carbonfields preview system

--- source/entrypoints/wp-admin.ts
+++ source/entrypoints/wp-admin.ts
@@ -1,1 +1,14 @@
 import '../styles/wp-admin.scss';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const setMinHeight = () => {
+        const previews = document.querySelectorAll<HTMLElement>('.cf-block__preview');
+        previews.forEach((preview) => {
+            if (preview.querySelector('.components-placeholder')) return;
+            const contentHeight = preview.querySelector('div')?.scrollHeight || 0;
+            preview.style.minHeight = `${contentHeight}px`;
+        });
+    };
+
+    setInterval(setMinHeight, 500);
+});
