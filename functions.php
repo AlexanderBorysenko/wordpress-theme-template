7970d9506ce31123c481a7f5e46b70bf6fa409bc
AlexanderBorysenko <borysenko.alexander@gmail.com>
2024-07-25 13:27:02 +0300

    Add dev tools connection

--- functions.php
+++ functions.php
@@ -8,12 +8,16 @@
  * @package wp_theme
  */
 
-require_once (get_template_directory() . '/helpers/require-all.php');
+require_once(get_template_directory() . '/helpers/require-all.php');
 require_all('helpers');
 
 
-require_once (get_template_directory() . '/vendor/autoload.php');
-require_once (get_template_directory() . '/source/components-core.php');
+require_once(get_template_directory() . '/vendor/autoload.php');
+require_once(get_template_directory() . '/source/components-core.php');
 
 require_all('includes');
-require_once (get_template_directory() . '/carbon/init.php');
+require_once(get_template_directory() . '/carbon/init.php');
+
+if (is_admin()) {
+    DevTools\DevTools::getInstance();
+}
